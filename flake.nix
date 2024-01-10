23cdb01eb9b6a1b7bf09d5481654505ea388fc02
peter louvel <peterlouvel@gmail.com>
2024-01-10 10:51:33 +1000

    Adding nix files to repo

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flake of nixy";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let 
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.nixy = lib.nixosSystem {
+      inherit system;
+      modules = [ ./configuration.nix ];
+    };
+    homeConfigurations.peter = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home.nix ];
+    };
+  };
+
+}
---
+++ update.sh
@@ -0,0 +1,5 @@
+cd ~/.dotfiles
+home-manager switch --flake .
+nix flake update
+sudo nixos-rebuild switch --flake .
+
